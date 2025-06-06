c3f28200780663a2f468e9d44a9c75165c4689c7
khuyn321 <khuyn321@gmail.com>
2025-06-05 21:43:41 -0700

    added player down sprite and its proper positioning

--- index.js
+++ index.js
@@ -9,8 +9,25 @@ context.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = 'Game_Resources/My_Game_Assets/My_Pokemon_Game_Map.png'
-console.log(image)
+
+const playerImage = new Image()
+playerImage.src = 'Game_Resources/Character_Movement/playerDown.png'
+
 
 image.onload = () => {
-  context.drawImage(image, -1320, -580)
+  context.drawImage(image, -1315, -580)
+  context.drawImage(
+    playerImage,
+    //----- cropp -----
+    0, // x-coord that we want to begin cropping from (top left)
+    0, // y-coord that we want to begin cropping from (top left)
+    playerImage.width / 4, // crop width: 1/4 of the 4 clones
+    playerImage.height,
+    //----- actual -----
+
+    (canvas.width / 2) - ((playerImage.width / 4) / 2),
+    (canvas.height / 2) - (playerImage.height / 2),
+    playerImage.width / 4, // crop width: 1/4 of the 4 clones
+    playerImage.height,
+  )
 }
