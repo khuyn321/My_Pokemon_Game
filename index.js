42aab1262618c0126dc42d0f7e4de91e11f591ce
khuyn321 <khuyn321@gmail.com>
2025-06-12 15:54:03 -0700

    started writing character animation code

--- index.js
+++ index.js
@@ -13,21 +13,47 @@ image.src = 'Game_Resources/My_Game_Assets/My_Pokemon_Game_Map.png'
 const playerImage = new Image()
 playerImage.src = 'Game_Resources/Character_Movement/playerDown.png'
 
+let backgroundImageX = -785
+let playerImageX = -785
 
-image.onload = () => {
+function animate() { //movement animation loop function
+  window.requestAnimationFrame(animate) //infinite loop
+
   context.drawImage(image, -1315, -580)
   context.drawImage(
     playerImage,
-    //----- cropp -----
+
+
+    //----- cropped -----
     0, // x-coord that we want to begin cropping from (top left)
     0, // y-coord that we want to begin cropping from (top left)
     playerImage.width / 4, // crop width: 1/4 of the 4 clones
     playerImage.height,
-    //----- actual -----
 
+
+    //----- actual -----
     (canvas.width / 2) - ((playerImage.width / 4) / 2),
     (canvas.height / 2) - (playerImage.height / 2),
     playerImage.width / 4, // crop width: 1/4 of the 4 clones
     playerImage.height,
   )
 }
+animate()
+
+window.addEventListener('keydown', (e) => {
+  // console.log(e.key)
+  switch (e.key) {
+    case 'w':
+      console.log('pressed w key')
+      break
+    case 'a':
+      console.log('pressed a key')
+      break
+    case 's':
+      console.log('pressed s key')
+      break
+    case 'd':
+      console.log('pressed d key')
+      break
+  }
+})
